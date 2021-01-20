6f36d155c37f630adcfa02654bf9aaae61ab7624
liyy <ants_double@yeah.net>
2021-01-20 10:45:20 +0800

    13

---
+++ algorithm/src/main/java/leetcode/RomanToInteger.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2021/1/20
+ */
+public class RomanToInteger {
+    public static void main(String[] args) {
+
+        int iii = romanToInt("III");
+        System.out.println(iii);
+    }
+
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int len = s.length(), result = map.get(s.charAt(len - 1));
+        for (int i = len - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) >= map.get(s.charAt((i + 1)))) {
+                result += map.get(s.charAt(i));
+            } else {
+                result -= map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
