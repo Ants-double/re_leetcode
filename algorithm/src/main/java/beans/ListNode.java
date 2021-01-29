9bbee3b8975aa55807f47ed4193a69cef80142a9
liyy <ants_double@yeah.net>
2021-01-29 13:26:03 +0800

    19

---
+++ algorithm/src/main/java/beans/ListNode.java
@@ -0,0 +1,42 @@
+package beans;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2021/1/29
+ */
+
+public class ListNode {
+    public ListNode next;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
