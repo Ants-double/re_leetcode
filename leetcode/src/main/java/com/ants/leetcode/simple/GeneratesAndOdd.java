086c8c3ada8f2711f465dcaa41a53999cda7303d
forein <ants_double@qq.com>
2022-08-01 09:35:58 +0800

    simple

---
+++ leetcode/src/main/java/com/ants/leetcode/simple/GeneratesAndOdd.java
@@ -0,0 +1,30 @@
+package com.ants.leetcode.simple;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author ants_
+ */
+public class GeneratesAndOdd {
+    public static void main(String[] args) {
+        GeneratesAndOdd test =new GeneratesAndOdd();
+        String s = test.generateTheString(5);
+        System.out.println(s);
+    }
+
+    public String generateTheString(int n) {
+
+        List<String> a = Collections.nCopies(4, "a");
+
+        StringBuffer sb=new StringBuffer();
+        if ((n&1)==1){
+            return Collections.nCopies(n,"a").stream().collect(Collectors.joining());
+        }
+        return Collections.nCopies(n-1,"a").stream().collect(Collectors.joining())+"b";
+
+
+
+    }
+}
